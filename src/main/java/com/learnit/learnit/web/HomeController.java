31b79f06dee53e4de7e62f373054520e8b74cb96
Jayed07 <47942152+Jayed07@users.noreply.github.com>
2021-03-25 16:48:08 +0200

    Home page updated.

--- src/main/java/com/learnit/learnit/web/HomeController.java
+++ src/main/java/com/learnit/learnit/web/HomeController.java
@@ -1,9 +1,11 @@
 package com.learnit.learnit.web;
 
+import com.learnit.learnit.repository.ArticleRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,14 +14,23 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class HomeController {
 
+    private final ArticleRepository articleRepository;
+
+    public HomeController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
     @GetMapping("/")
     public String index() {
         return "index";
     }
 
     @GetMapping("/home")
-    public String home() {
-        return "index";
+    public String home(Model model) {
+
+        model.addAttribute("articles", articleRepository.findAll());
+
+        return "home";
     }
 
     @GetMapping("/logout")
